40c4a8609aec6e89d64da1ad538d79fe437927da
yulei <xybbw@126.com>
2016-10-22 17:28:47 +0800

    放开权限相关前端页面

--- WebRoot/template/default/xtsz/leftMenu.ftl
+++ WebRoot/template/default/xtsz/leftMenu.ftl
@@ -3,20 +3,11 @@
     <li <#if CURENT_TAB_2 =='JBSZ'>class="active"</#if>>
         <a href="${BASE_PATH}xtsz/jbsz.action"><#if CURENT_TAB_2 =='JBSZ'><span class="yu-tu-fgsj-active"></span><#else><span class="yu-tu-fgsj-commen"></span></#if>基本设置</a>
     </li>
-    <li <#if CURENT_TAB_2 =='CFSZ'>class="active"</#if>>
-        <a href="${BASE_PATH}xtsz/cfsz.action"><#if CURENT_TAB_2 =='CFSZ'><span class="yu-tu-fgsj-active"></span><#else><span class="yu-tu-fgsj-commen"></span></#if>仓房设置</a>
-    </li>	
-    <li <#if CURENT_TAB_2 =='LQPZ'>class="active"</#if>>
-        <a href="${BASE_PATH}xtsz/cssz.action"><#if CURENT_TAB_2 =='LQPZ'><span class="yu-tu-fgsj-active"></span><#else><span class="yu-tu-fgsj-commen"></span></#if>粮情配置</a>
-    </li>
+   
     <li <#if CURENT_TAB_2 =='YHGL'>class="active"</#if>>
         <a href="${BASE_PATH}user/yhgl.action"><#if CURENT_TAB_2 =='YHGL'><span class="yu-tu-fgsj-active"></span><#else><span class="yu-tu-fgsj-commen"></span></#if>用户管理</a>
     </li>
-    <#if hasVentilate> 
-    <li <#if CURENT_TAB_2 =='ZNTF'>class="active"</#if>>
-        <a href="${BASE_PATH}xtsz/tfsb.action"><#if CURENT_TAB_2 =='ZNTF'><span class="yu-tu-fgsj-active"></span><#else><span class="yu-tu-fgsj-commen"></span></#if>通风配置</a>
-    </li>
-    </#if>
+
     <li <#if CURENT_TAB_2 =='GRSZ'>class="active"</#if>>
         <a href="${BASE_PATH}user/personDetail.action?oType=detail"><#if CURENT_TAB_2 =='GRSZ'><span class="yu-tu-fgsj-active"></span><#else><span class="yu-tu-fgsj-commen"></span></#if>个人设置</a>
     </li>
---
+++ src/com/spring/controller/XtszController.java
@@ -0,0 +1,77 @@
+package com.spring.controller;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.common.consts.PageConst;
+import com.common.consts.WebConstConfig;
+import com.common.utils.CalendarUtil;
+import com.common.utils.CommonUtils;
+import com.common.utils.GenerateSequenceUtil;
+import com.common.utils.RequestUtil;
+import com.common.utils.StringUtils;
+import com.ftlTemplates.FreeMarkerUtil;
+import com.hibernate.baseSettingInfo.domain.AlarmBean;
+import com.hibernate.baseSettingInfo.domain.BaseSettingBean;
+import com.hibernate.baseSettingInfo.domain.GrainTypeBean;
+import com.hibernate.timers.domain.TimerTaskBean;
+import com.hibernate.userInfo.damain.RoleBean;
+import com.hibernate.userInfo.damain.User;
+import com.spring.ServiceManager;
+@Controller
+public class XtszController {
+    private static Logger logger = Logger.getLogger(Logger.class);
+    /**
+     * 浠撴埧璁剧疆
+     * 
+     * @param request
+     * @param response
+     * @param model
+     * @return
+     */
+    @RequestMapping({ "/xtsz/jbsz.action", "/xtsz.action" })
+    public ModelAndView barnSetting(HttpServletRequest request,
+            HttpServletResponse response, ModelMap model) {
+        try {
+            User user = (User) request.getSession().getAttribute("user");
+            if (null != user) {
+                model.addAttribute("theme", user.getTheme());
+            }
+            model.addAttribute("sensorList", null);
+            model.addAttribute("grainList", null);
+            // 妯℃澘璺¯寰 basePath
+            model.addAttribute("BASE_PATH", WebConstConfig.BASE_PATH);
+            model.addAttribute("BASE_ASSETS_PATH",
+                    WebConstConfig.getBase_Assets_Path());
+            model.addAttribute("BASE_TEMPLATE_DEFAULT_PATH",
+                    WebConstConfig.getBase_Template_Default_Path());
+            // model.addAttribute("user",request.getSession().getAttribute("user"));
+            model.addAttribute("CURENT_TAB", "XTSZ");
+            model.addAttribute("CURENT_TAB_2", "JBSZ");
+            return new ModelAndView(PageConst.XTSZ_JBSZ, model);
+        } catch (Exception e) {
+            e.printStackTrace();
+            model.addAttribute("error", e.getMessage());
+            return null;
+        }
+    }
+ 
+}
