898ad11350efdbb1bf2edab0d071834cc2ee8b6c
lixb <lixb@longbei.com>
2016-11-15 14:43:24 +0800

    no message

--- src/com/spring/controller/IndexController.java
+++ src/com/spring/controller/IndexController.java
@@ -157,6 +157,7 @@ public class IndexController {
 				userPwd = Md5.getMD5(userPwd.getBytes());
 			}
 			List<User> userList = ServiceManager.getUserService().getByName(userName);
+			ServiceManager.getHouseBasicServce().updateAge();
 			User user = null;
 			if (userList != null && userList.size() > 0) {
 				user = userList.get(0);
